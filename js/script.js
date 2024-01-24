6fd32ec9a648101a64b3078df64dee779c9aa4af
emilianogastaldo <emiliano0303@gmail.com>
2024-01-24 16:02:22 +0100

    created vue app and added datas

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log(Vue);
+
+const { createApp } = Vue;
+
+const app = createApp({
+    name: 'Boolzapp',
+    data: () => ({
+        prova: 'Prova collegamento Vue',
+        user,
+        contacts
+    }),
+});
+
+app.mount('#root')
